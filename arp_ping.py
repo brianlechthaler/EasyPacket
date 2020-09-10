a55c55bc8edc2b41f7f8879696cb2435ddc4b4d0
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-09-10 10:36:18 -0700

    First function: ARP Ping!

---
+++ arp_ping.py
@@ -0,0 +1,10 @@
+from scapy.all import *
+
+destIP = "127.0.0.1"
+dest = destIP
+
+def arpPing(dest):
+    msg = "Sending ARP Ping Packets to: " + dest
+    print(msg)
+    arppinger = sr(IP(dst=dest, proto=(1,254)))
+    arppinger()
